ae65deb35ed6c5cdb7c467ed6acedb1c24386eed
Bohyeon Kim <love.free.work@gmail.com>
2022-08-19 14:06:44 +0900

    first commit:

---
+++ app.js
@@ -0,0 +1,53 @@
+// express 모듈을 불러오고, 보안(CORS),포트 등 환경 초기화
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const Http = require("http");
+const Https = require("https");
+
+// 환경변수 모듈 불러오기 (process.env. + 변수 설정) -> process.env. 객체 사용 가능
+const dotenv = require("dotenv");
+dotenv.config();
+
+// express 객체 선언, 각종 middleware 설치
+const app = express();
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// CORS OPTION 적용
+const corsOption = {
+  origin: ["http://localhost:3000", "http://nodeapi.myspaceti.me:8002"],
+  credentials: true,
+};
+app.use(cors(corsOption));
+
+// mongoDB에 연결
+const connect = require("./schemas/index.js");
+connect();
+// "/api" path로 연결하는 라우터 연결 (우선 routes/index.js로)
+const indexRouter = require("./routes/index.js");
+app.use("/api", [indexRouter]);
+
+// HTTPS 연결 관련
+const fs = require("fs");
+const options = {
+  key: fs.readFileSync("./ssl/www_myspaceti.me.key"),
+  cert: fs.readFileSync("./ssl/www_myspaceti.me_cert.crt"),
+  ca: fs.readFileSync("./ssl/www_myspaceti.me_chain_cert.crt"),
+};
+const http = Http.createServer(app);
+const https = Https.createServer(options, app);
+const http_port = process.env.HTTP_PORT || 3000;
+const https_port = process.env.HTTPS_PORT || 443;
+
+// SERVER LISTEN
+http.listen(http_port, () => {
+  console.log(`Start listen Server: ${http_port}`);
+});
+
+https.listen(https_port, () => {
+  console.log(`Start listen Server: ${https_port}`);
+});
+
+module.exports = app;
---
+++ schemas/post.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose"); // 몽구스를 사용하겠다는 선언
+
+// 몽구스로 post라는 객체는 이런 모양으로 받겠다고 선언
+const PostSchema = new mongoose.Schema({
+  user_id: {
+    type: String, // 이건 문자열로 써야 합니다.
+    required: true, // 필수로 들어와야 합니다.
+  },
+  CONTENT: {
+    type: String,
+    required: true,
+  },
+  POST_PHOTO: {
+    type: String,
+    required: true,
+  },
+  TIMESTAMPS: {
+    type: Date, // 이건 날짜 형태로 받을게요~!
+    required: true,
+  },
+});
+
+// 이로써 posts 관련 DB는, 여기서 만든 몽구스 모델을 기준으로 받겠다고 외부에 선언/공개합니다.
+module.exports = mongoose.model("Posts", PostSchema);
