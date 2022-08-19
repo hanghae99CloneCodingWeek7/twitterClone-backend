8c9eb711a19955dbe38b4dfa9acefad8c19699c0
Bohyeon Kim <love.free.work@gmail.com>
2022-08-19 19:50:49 +0900

    Make login api done

--- schemas/user.js
+++ schemas/user.js
@@ -3,20 +3,18 @@ const mongoose = require("mongoose"); // 몽구스를 사용하겠다는 선언
 // 몽구스로 post라는 객체는 이런 모양으로 받겠다고 선언
 const UserSchema = new mongoose.Schema({
   USER_ID: {
-    type: String, // 이건 문자열로 써야 합니다.
-    required: true, // 필수로 들어와야 합니다.
-  },
-  PASSWORD: {
     type: String,
     required: true,
+    unique: true,
   },
-  PASSWORD_SALT: {
+  PASSWORD: {
     type: String,
     required: true,
   },
   EMAIL: {
     type: String,
     required: true,
+    unique: true,
   },
   FOLLOWER: {
     type: Array,
